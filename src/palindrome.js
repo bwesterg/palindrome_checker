831e4a28e4fda629794ab8d4206561c471edaadc
bwesterg <benton.westergaard@gmail.com>
2023-11-30 13:45:41 -0700

    improves logic and notes for palindrome checker.

--- src/palindrome.js
+++ src/palindrome.js
@@ -1,29 +1,18 @@
-// console.log('hello');
+function palindromeChecker(str){
+    let cleaned = str.replace(/\W/g, '').toLowerCase();
+    //takes original string and removes non-alphanumeric noise,
+    //then converts all characters to lowercase.
 
-//function to test if a string is a palindrome
-
-function palindromeChecker(str) {
-    //remove non-alphanumeric characters
-    // clean = str.replace(/[^a-zA-Z0-9]/g, "");
-    // lowered = clean.toLowerCase();
-    // reversed = lowered.split('').reverse().join('');
-    // console.log(lowered, reversed);
-
-    let cleaned = str.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
     let reversed = cleaned.split('').reverse().join('');
+    //sends each character to an array, reverses the array
+    //order,and rejoins characters to a new string.
+
     console.log(cleaned, reversed);
-
-        if (reversed === cleaned){
-            console.log(str + " is a palindrome, after removing non-alphanumeric characters and making all letters lowercase.");
-        } else {
-            console.log(str + " is NOT a palindrome");
-        }
-
-    // if (lowered === reversed){
-    //     console.log("it is a palindrome");
-    // } else {
-    //     console.log("NOT a palindrome");
-    // }
+    if (reversed === cleaned) {
+        console.log("IS a palindrome");
+    } else {
+        console.log("NOT a palindrome")
+    };
 };
 
-palindromeChecker("Ra%cecar");
+palindromeChecker("taco cat")
