6393d98254be1943c99b95296c30a6eeffdeb3a0
bwesterg <benton.westergaard@gmail.com>
2023-12-04 23:10:09 -0700

    removes space

--- src/palindrome.js
+++ src/palindrome.js
@@ -2,11 +2,9 @@ function palindromeChecker(str){
     let cleaned = str.replace(/\W/g, '').toLowerCase();
     //takes original string and removes non-alphanumeric noise,
     //then converts all characters to lowercase.
-
     let reversed = cleaned.split('').reverse().join('');
     //sends each character to an array, reverses the array
     //order,and rejoins characters to a new string.
-
     console.log(cleaned, reversed);
     if (reversed === cleaned) {
         console.log("IS a palindrome");
