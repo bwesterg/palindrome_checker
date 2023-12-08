a2e998e141ee4389b8405942156de2a5b7c14d77
bwesterg <benton.westergaard@gmail.com>
2023-12-07 22:55:04 -0700

    adds frankensplice problem

--- src/palindrome.js
+++ src/palindrome.js
@@ -97,3 +97,24 @@ function titleCase(str) {
 }
 
 titleCase("I'm a little tea pot");
+
+//Copy each element of the first array into the second 
+//array, in order.
+
+//Begin inserting elements at index n of the second array.
+
+//Return the resulting array. The input arrays should 
+//remain the same after the function runs.
+
+function frankenSplice(arr1, arr2, n) {
+    let arr2copy = arr2.slice();
+    //console.log(arr2copy);
+    for (let i=0;i<arr1.length;i++){
+        arr2copy.splice(n,0,arr1[i]);
+        n++;
+    }
+    console.log(arr2copy)
+    return arr2copy; 
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
